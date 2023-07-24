9f775b2ddc014ec851dc5031d5527b9f93d6d671
sitp-jose <jg.lumbao@seiko-it.com.ph>
2023-07-24 16:12:31 +0800

    Add project files.

---
+++ CRUD-APP/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_APP.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+
+    }
+}
